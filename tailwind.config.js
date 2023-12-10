e846681250136694a7b5624d6c74c5a02bc394da
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2023-12-10 21:59:41 +0100

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: [],
+  darkMode: false,
   theme: {
     extend: {
-      fontFamily: {
-        archivo: "Archivo",
-        latoLight: "LatoLight",
-        latoBold: "LatoBold",
-        latoReg: "LatoReg",
+      colors: {
+        primary: '#3490dc',
+        secondary: '#ffed4a',
+        accent: {
+          100: '#f7f7f7',
+          200: '#eaeaea',
+          300: '#999999',
+        },
       },
-      height: {
-        "10v": "10vh",
-        "20v": "20vh",
-        "30v": "30vh",
-        "40v": "40vh",
-        "50v": "50vh",
-        "60v": "60vh",
-        "70v": "70vh",
-        "80v": "80vh",
-        "90v": "90vh",
-        "100v": "100vh",
+      fontSize: {
+        '2xs': '0.625rem',
+        '3xs': '0.5rem',
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      },
+      boxShadow: {
+        'custom': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      },
+      borderWidth: {
+        '3': '3px',
+        '6': '6px',
+      },
+      borderRadius: {
+        'xl': '1rem',
+        '2xl': '2rem',
+      },
+      zIndex: {
+        '60': 60,
+        '70': 70,
       },
     },
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-};
+}
+
