b212ad28253665ec257acc953883edccb2d03047
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2024-01-08 21:26:15 +0100

    initial commit

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,9 +1,9 @@
-import { motion as m } from "framer-motion";
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 export default function Error() {
   return (
-    <m.main
+    <motion.main
       animate={{ y: "0%" }}
       exit={{ opacity: 1 }}
       initial={{ y: "100%" }}
@@ -11,21 +11,21 @@ export default function Error() {
         duration: 0.75,
         ease: "easeOut",
       }}
-      className="text-white absolute top-0 left-0 w-full h-full bg-black lg:px-48 px-16"
+      className="absolute top-0 left-0 w-full h-full bg-black lg:px-48 px-16"
     >
       <div className="my-96 p-1 overflow-hidden ">
-        <m.h1
+        <motion.h1
           animate={{ y: 0 }}
           initial={{ y: "100%" }}
           transition={{ delay: 0.5, duration: 0.5 }}
           className="lg:text-9xl text-6xl lg:text-right text-center"
         >
           Whoops something went wrong.
-        </m.h1>
+        </motion.h1>
         <Link href={"/"} className="hover:cursor-pointer">
           Go back to Home
         </Link>
       </div>
-    </m.main>
+    </motion.main>
   );
 }
